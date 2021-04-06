af7a2650eda6fba8995e741376ab2f81f6983896
אמיר דגן <amir.dgn@outlook.com>
2021-04-06 21:32:07 +0300

    Add project files.

---
+++ CatchTheDot/10_neoPixelControl.ino
@@ -0,0 +1,39 @@
+
+#include <Adafruit_NeoPixel.h>
+#define MATRIX_ROWS 8
+#define MATRIX_COLS 8
+#define MATRIX_TOTAL 64
+
+#define PIN 6         // Parameter 1 = number of pixels in strip
+// Parameter 2 = pin number (most are valid)
+// Parameter 3 = pixel type flags, add together as needed:
+// NEO_KHZ800 800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
+// NEO_KHZ400 400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
+// NEO_GRB Pixels are wired for GRB bitstream (most NeoPixel products)
+// NEO_RGB Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(MATRIX_TOTAL, PIN, NEO_GRB + NEO_KHZ800);
+#define BOARD_PIX_IN_ROW 7
+#define BOARD_PIX_IN_COL 6
+byte board[BOARD_PIX_IN_COL][BOARD_PIX_IN_ROW];
+int dropOnMsec = 250;
+uint32_t clrPlayer[3] = { 0,strip.Color(0, 30, 30),strip.Color(30, 0, 30) };
+void slowLightAll(uint32_t clr) {
+    for (int pixelNum = 0; pixelNum < MATRIX_TOTAL; pixelNum++) {
+        strip.setPixelColor(pixelNum, clr);
+        strip.show();
+        delay(20);
+    }
+}
+void slowOffAll() {
+    for (int pixelNum = 0; pixelNum < MATRIX_TOTAL; pixelNum++) {
+        strip.setPixelColor(pixelNum, strip.Color(0, 0, 0));
+        strip.show();
+        delay(40);
+    }
+    delay(200);
+}
+void lightPixel(int row, int col, uint32_t clr) {
+    int pixelNum = row * MATRIX_COLS + col;
+    strip.setPixelColor(pixelNum, clr);
+    strip.show();
+}
---
+++ CatchTheDot/CatchTheDot.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		CatchTheDot.ino
+ Created:	4/6/2021 8:05:50 PM
+ Author:	amird
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
